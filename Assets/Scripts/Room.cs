dc9597a1cb7e90f8c8cc8874e78d564acc42c57a
Nuno Carriço <nuno.carrico.98@gmail.com>
2019-06-18 22:25:19 +0100

    Add death panel

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -11,19 +11,38 @@ public class Room : MonoBehaviour
 	[SerializeField] private float hpPerObject;
 	[SerializeField] private float hpRemovalInterval;
 
+	[SerializeField] private GameObject deathPanel;
+
 	private float hpToRemove = 0;
 	private float roomHP;
 
 	public float HPRemovalInterval => hpRemovalInterval;
+	public bool Dead { get; private set; } = false;
 
 	private void Start()
 	{
 		roomHP = maxRoomHP;
 	}
 
+	private void Update()
+	{
+		Die();
+	}
+
+	public void Die()
+	{
+		if (roomHP <= 0)
+		{
+			Dead = true;
+			deathPanel.SetActive(true);
+			StopAllCoroutines();
+		}
+	}
+
 	public void SetRoomHP()
 	{
 		roomHP -= hpToRemove;
+		if (roomHP < 0) roomHP = 0;
 		float ratio = roomHP / maxRoomHP;
 		Sequence setBars = DOTween.Sequence();
 		setBars.Append(healthBar.transform.DOScaleY(ratio, .2f))
